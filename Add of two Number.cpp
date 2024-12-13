c86da697e182009f698c38f6ae78422a261933f6
Rishabh93Mishra <24150cn104@glbitm.ac.in>
2024-12-13 19:49:03 +0530

    Add files via upload

---
+++ Add of two Number.cpp
@@ -0,0 +1,13 @@
+//Input Two Number And ADD Numbers
+#include<stdio.h>
+int main()
+{
+	int a,b,add;
+	printf("Enter Two Number :\n");
+	scanf("%d %d",&a,&b);
+	add = a+b;
+	printf("The Sum Of Two Number is :%d ",add);
+	return 0;
+	
+}
+
